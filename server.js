e0b328cd271197c31d465379fc161839da1f734e
luisalbertoj <luisalbertoj.tober@gmail.com>
2020-01-04 13:31:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ res.sendFile(path.join(__dirname,'/dist/publiClickFrontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
+/* app.listen(8080); */
